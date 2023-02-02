b358fc428f93d63849e2e7c7b8f4ddadde2d7120
almerk <almerk.work@gmail.com>
2023-02-02 15:45:38 +0300

    Wrote first (simple) implementation

---
+++ GuidBenchmark/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+BenchmarkRunner.Run<GuidBenchmark>();
+
+[MemoryDiagnoser(displayGenColumns: false)]
+public class GuidBenchmark 
+{
+    private static readonly Guid Guid = Guid.Parse("1d3e6ce5-9327-4ba5-aa3c-27e2520ad0ef");
+    const string GuidStr = "5Ww_HSeTpUuqPCfiUgrQ7w";
+
+    [Benchmark]
+    public string ToStringSimpleB() => ToStringSimple(Guid);
+    [Benchmark]
+    public Guid FromStringSimpleB () => FromStringSimple(GuidStr);
+
+
+    static string ToStringSimple(Guid guid)
+    {
+        return Convert.ToBase64String(guid.ToByteArray())
+            .Replace('+','_')
+            .Replace('/','-')
+            .TrimEnd('=');
+    }
+
+    static Guid FromStringSimple(string base64)
+    {
+        var mapped = base64.Replace('_', '+').Replace('-','/') + "==";
+
+        return new Guid(Convert.FromBase64String(mapped));
+    }
+}
+
+
+
---
+++ GuidConversion/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace GuidConversion;
+public static class Extensions
+{
+    static string ToUrlString(this Guid guid)
+    {
+        return Convert.ToBase64String(guid.ToByteArray())
+            .Replace('+','_')
+            .Replace('/','-')
+            .TrimEnd('=');
+    }
+
+    static Guid FromUrlString(string base64)
+    {
+        var mapped = base64.Replace('_', '+').Replace('-','/') + "==";
+
+        return new Guid(Convert.FromBase64String(mapped));
+    }
+}
